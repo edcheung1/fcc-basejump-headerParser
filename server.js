7c609b63cfb3fb58d095663978647f8f8148c2dd
Ed <echeung1991@gmail.com>
2016-01-15 18:39:04 -0600

    blah

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express'),
-	routes = require(./app/routes/index.js);	
+	routes = require('./app/routes/index.js');	
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -12,3 +12,4 @@ routes(app);
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port ', app.get('port'));	
 });
+
